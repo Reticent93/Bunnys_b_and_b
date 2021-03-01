f293a44adf5c55d06bf9a2e97d2e4abcde5fca3a
Greg <remph819@gmail.com>
2021-03-01 07:52:34 -0700

    Added html files

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,18 +7,13 @@ import (
 	"net/http"
 )
 
-
 // Repo the repository used by the handlers
 var Repo *Repository
-
 
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
 }
-
-
-
 
 // NewRepo creates a new repository
 func NewRepo(a *config.AppConfig) *Repository {
@@ -32,32 +27,23 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-
 // Home is the homepage handler
-func (m *Repository) Home(w http.ResponseWriter, r *http.Request)  {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.tmpl.html", &models.TemplateData{})
 }
 
-
 // About is the about page handler
-func (m *Repository) About(w http.ResponseWriter, r *http.Request)  {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//  perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again World"
-	
+
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 	// send the data to the template
 	render.RenderTemplate(w, "about.page.tmpl.html", &models.TemplateData{
 		StringMap: stringMap,
 	})
-	
-	
-
-	
 }
-
-
-
